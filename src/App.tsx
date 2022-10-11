1b68c4003e96a8cf0cea3d3552eab784d7d327a0
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-10-11 19:35:25 +0200

    main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,32 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <main>
+        <header>
+          <h3>StudyH</h3>
+          <ul>
+            <li>LogIn</li>
+            <button>DMS</button>
+          </ul>
+        </header>
+        <section className="exercises-section">
+          <h4>Exercises section</h4>
+        </section>
+        <section className="ranking-section">
+          <input className="search" placeholder="Search Question"></input>
+          <ul>
+          students ranking
+          <li>Student1 bananan scores</li>
+          <li>Student2 bananan scores</li>
+          <li>Student3 bananan scores</li>
+          </ul>
+        </section>
+      </main>
     </div>
   )
 }
