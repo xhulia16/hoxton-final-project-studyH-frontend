0ef75355b325f12a4363f4b2ae899aecc2599078
XHULIA PRISHKA <xh.prishka@gmail.com>
2022-10-12 13:08:29 +0200

    commit

--- src/components/Classmates.tsx
+++ src/components/Classmates.tsx
@@ -1,10 +1,11 @@
 export function ClassMates() {
   return (
     <ul className="classmates-list">
-      <li>ffgfgf</li>
-      <li>dfgdhfgsdhfdsjhf</li>
-      <li>dfgdhfgsdhfdsjhf</li>
-      <li>dfgdhfgsdhfdsjhf</li>
+      <p>Your Classmates</p>
+      <li>
+        <img className="profile-pic" src="https://twt-thumbs.washtimes.com/media/image/2020/08/17/tv_disney_channel_bisexual_73750_c34-0-1886-1080_s885x516.jpg?deb7d5ab31b4d71690149fe98c14a0bbc2f9bcee"></img>
+        <p>Classmate Name</p>
+      </li>
     </ul>
   );
 }
--- src/components/Rankings.tsx
+++ src/components/Rankings.tsx
@@ -1,13 +1,34 @@
-export function Ranking(){
-    return(
-        <section className="ranking-section">
-          <input className="search" placeholder="Search Question"></input>
-          <ul>
-          students ranking
-          <li>Student1 bananan scores</li>
-          <li>Student2 bananan scores</li>
-          <li>Student3 bananan scores</li>
-          </ul>
-        </section>
-    )
+export function Ranking() {
+  return (
+    <section className="ranking-section">
+      <input className="search" placeholder="Search Question"></input>
+      <ul>
+        <h3>Highest Scores</h3>
+        <li className="pupil-rank">
+          <p className="ranking">#1</p>
+          <img
+            className="profile-pic"
+            src="https://twt-thumbs.washtimes.com/media/image/2020/08/17/tv_disney_channel_bisexual_73750_c34-0-1886-1080_s885x516.jpg?deb7d5ab31b4d71690149fe98c14a0bbc2f9bcee"
+          ></img>
+          <p>Classmate Name</p>
+        </li>
+        <li className="pupil-rank">
+          <p className="ranking">#2</p>
+          <img
+            className="profile-pic"
+            src="https://twt-thumbs.washtimes.com/media/image/2020/08/17/tv_disney_channel_bisexual_73750_c34-0-1886-1080_s885x516.jpg?deb7d5ab31b4d71690149fe98c14a0bbc2f9bcee"
+          ></img>
+          <p>Classmate Name</p>
+        </li>
+        <li className="pupil-rank">
+          <p className="ranking">#3</p>
+          <img
+            className="profile-pic"
+            src="https://twt-thumbs.washtimes.com/media/image/2020/08/17/tv_disney_channel_bisexual_73750_c34-0-1886-1080_s885x516.jpg?deb7d5ab31b4d71690149fe98c14a0bbc2f9bcee"
+          ></img>
+          <p>Classmate Name</p>
+        </li>
+      </ul>
+    </section>
+  );
 }
